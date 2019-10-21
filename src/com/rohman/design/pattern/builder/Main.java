bdbc9b7fbcbd9f5723c057143ef35cba182cab33
Rohmanseo <rohmann311@gmail.com>
2019-10-21 21:21:57 +0700

    Menambahkan Dosen Builder

--- src/com/rohman/design/pattern/builder/Main.java
+++ src/com/rohman/design/pattern/builder/Main.java
@@ -8,7 +8,23 @@ public class Main {
                 .setNIM(1689)
                 .build();
 
+        Dosen dosen = new Dosen.Builder()
+                .setNama("Dosen 1")
+                .setNIDN(1234)
+                .setMataKuliah("Matematika")
+                .setTanggalLahir("28 Februari")
+                .build();
+        Dosen dosen2 = new Dosen.Builder()
+                .setNama("Dosen 2")
+                .setNIDN(1234)
+                .setMataKuliah("Bahasa Indonesia")
+                .setTanggalLahir("28 Februari")
+                .build();
+
         System.out.println(mahasiswa);
+        System.out.println(dosen);
+        System.out.println(dosen2);
+
     }
 
 }
